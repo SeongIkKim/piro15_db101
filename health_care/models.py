960f3d9fe4bf8795e4ea14b40af5a548e5aa5ab4
SeongIkKim <kpic1638@gmail.com>
2021-07-15 02:07:28 +0900

    <add> Member Entity

--- health_care/models.py
+++ health_care/models.py
@@ -9,9 +9,16 @@ class FitnessClub(models.Model):
         return self.name
 
 class Trainer(models.Model):
-    club = models.ForeignKey(verbose_name="소속 헬스장", to='health_care.FitnessClub', on_delete=models.SET_NULL, null=True)
+    club = models.ForeignKey(verbose_name="소속 헬스장", to='health_care.FitnessClub',
+                             on_delete=models.SET_NULL, null=True)
     name = models.CharField(verbose_name="이름", max_length=50)
     phone_num = models.CharField(verbose_name="휴대폰번호", max_length=12)
 
-
-
+class Member(models.Model):
+    club = models.ForeignKey(verbose_name="소속 헬스장", to='health_care.FitnessClub',
+                             on_delete=models.CASCADE, related_name='member')
+    trainer = models.ForeignKey(verbose_name="전담 트레이너", to='health_care.Trainer',
+                                on_delete=models.SET_NULL, null=True, related_name='trainee')
+    name = models.CharField(verbose_name="이름", max_length=50)
+    phone_num = models.CharField(verbose_name="휴대폰번호", max_length=12)
+    expiry_date = models.DateField(verbose_name="만료일")
