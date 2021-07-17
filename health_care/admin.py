5cd2861204fce87b88f78731fb8d3991aef9ae0c
SeongIkKim <kpic1638@gmail.com>
2021-07-17 09:18:26 +0900

    <feat> admin site register

--- health_care/admin.py
+++ health_care/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from health_care.models import FitnessClub, Trainer, Member, PersonalTrain, Locker, Equipment, Use
+
+for model in [FitnessClub, Trainer, Member, PersonalTrain, Locker, Equipment, Use]:
+    admin.site.register(model)
